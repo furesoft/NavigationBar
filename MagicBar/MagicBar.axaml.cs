a0c395ccdc0322e254e0dce31104a003261e26c2
Chris Anders <filmee24@gmail.com>
2024-01-12 21:57:27 +0100

    refactor

--- MagicBar/MagicBar.axaml.cs
+++ MagicBar/MagicBar.axaml.cs
@@ -16,13 +16,5 @@ public class MagicBar : ListBox
     protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
     {
         partPresenter = e.NameScope.Find<ItemsPresenter>("PART_Presenter");
-
-        SelectionChanged += OnSelectionChanged;
-    }
-
-    private void OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
-    {
-        var k = (SelectedItem as ListBoxItem).TranslatePoint(new Point(0, 0), this);
-        //Canvas.SetLeft(partCircle, k.Value.X - 35);
     }
 }
